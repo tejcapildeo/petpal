4fd3db2bfa1c5e55f7081613bd4e92546bfa7486
Humraj Bhoday <humrajbhoday@Humrajs-MacBook-Air.local>
2023-11-15 11:39:41 -0500

    added profile pic to seeker

--- accounts/models.py
+++ accounts/models.py
@@ -19,6 +19,7 @@ class Seeker(models.Model):
     phone = models.CharField(max_length=20, blank=True, null=True)
     location = models.CharField(max_length=100, blank=True, null=True)
     preferences = models.TextField(blank=True, null=True)
+    profile_pic=models.ImageField(blank=True)
 
     def __str__(self):
         return self.user.username
